6bf0cec3e704eca0697da57c771da184c8a252f9
Masho Ado <mashoado@gmail.com>
2021-09-18 13:06:45 +0300

    remove menu when links are clicked on  mobile

--- public/src/main.js
+++ public/src/main.js
@@ -1,7 +1,22 @@
 const menu =document.getElementById("nav-menu")
 const menuButton = document.getElementById("menu-btn")
+const navLink = document.getElementsByClassName("nav-link")
+
+// destructe the html collection
+const navLinkarr = [...navLink]
+console.log(navLinkarr)
+
 
 menuButton.addEventListener("click", () =>{
     console.log("working")
     menu.classList.toggle("show")
 })
+
+// remove menu when link is clicked
+
+navLinkarr.forEach(link => {
+   link.addEventListener("click", ()=>{
+    menu.classList.remove("show")
+   })
+    
+});
